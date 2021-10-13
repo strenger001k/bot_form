26ff7b133ab55d9f8ef105ec378032398d86d31d
stranger001k <zhenia0072@gmail.com>
2021-10-13 15:24:48 +0300

    ok

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ def process_change_name(message):
             bot.register_next_step_handler(msg, process_change_name)
 
 
-@server.route('/' + TOKEN, methods=['POST'])
+"""@server.route('/' + TOKEN, methods=['POST'])
 def get_message():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
@@ -209,8 +209,8 @@ def get_message():
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
-    return '!', 200
+    return '!', 200"""
 
 
-#if __name__ == '__main__':
-    #server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
--- server.py
+++ server.py
@@ -1,7 +1,20 @@
-import os
+import telebot
 from flask import Flask, request
+from config import TOKEN, APP_URL
 
 server = Flask(__name__)
 
-if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
