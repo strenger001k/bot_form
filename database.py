6bb23fcf05c83aea57c7ca18198b25c342563ac0
stranger001k <zhenia0072@gmail.com>
2021-10-13 22:37:17 +0300

    Ok

--- database.py
+++ database.py
@@ -2,5 +2,5 @@ def add(dict, messege_text):
     dict.append(messege_text)
 
 
-def replace(dict, messege_text):
-    dict = messege_text
+def replace(dict, messege_text, index):
+    dict[index] = (messege_text)
