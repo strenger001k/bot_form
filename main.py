5a3e4d626d59a1b74720a8d6d466e6838460b9bf
stranger001k <zhenia0072@gmail.com>
2021-10-13 12:04:39 +0300

    Ok

--- main.py
+++ main.py
@@ -228,13 +228,11 @@ def get_message():
     bot.process_new_updates([update])
     return '!', 200
 
-
 @server.route('/')
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     return '!', 200
-
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
