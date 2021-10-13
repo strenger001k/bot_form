4738e0b70889e5b22bff4b7a123bacc0588ebfa1
stranger001k <zhenia0072@gmail.com>
2021-10-13 14:17:59 +0300

    Ok

--- main.py
+++ main.py
@@ -31,27 +31,24 @@ def send_message(message):
 
 
 def process_name(message, a):
-    try:
-        if(len(message.text) >= 2 and len(message.text)<=20):
-            if not a:
-                database.add(BD, message.chat.id, message.text)
-            else:
-                database.replace(BD, message.chat.id, message.text, 0)
-            markup.add(back)
-            msg = bot.send_message(message.from_user.id, "Ваш вік", reply_markup=markup)
-            if a==0:
-                bot.register_next_step_handler(msg, process_age, 0)
-            else:
-                bot.register_next_step_handler(msg, process_age, 1)
+    if(len(message.text) >= 2 and len(message.text)<=20):
+        if not a:
+            database.add(BD, message.chat.id, message.text)
         else:
-            bot.reply_to(message, "Імʼя від 2 до 20 символів")
-            msg = bot.send_message(message.from_user.id, "Ваше Ім'я")
-            if not a:
-                bot.register_next_step_handler(msg, process_name, 0)
-            else:
-                bot.register_next_step_handler(msg, process_name, 1)
-    except:
-        bot.reply_to(message, "Помилка(")
+            database.replace(BD, message.chat.id, message.text, 0)
+        markup.add(back)
+        msg = bot.send_message(message.from_user.id, "Ваш вік", reply_markup=markup)
+        if a==0:
+            bot.register_next_step_handler(msg, process_age, 0)
+        else:
+            bot.register_next_step_handler(msg, process_age, 1)
+    else:
+        bot.reply_to(message, "Імʼя від 2 до 20 символів")
+        msg = bot.send_message(message.from_user.id, "Ваше Ім'я")
+        if not a:
+            bot.register_next_step_handler(msg, process_name, 0)
+        else:
+            bot.register_next_step_handler(msg, process_name, 1)
 
 def process_age(message, a):
     if message.text == "Назад":
@@ -91,15 +88,12 @@ def process_gender(message):
         markup.add(back)
         msg = bot.send_message(message.from_user.id, "Ваш вік", reply_markup=markup)
         bot.register_next_step_handler(msg, process_age, 1)
-    elif message.text == "Чоловік" or message.text == "Дівчина":
-        try:
-            database.add(BD, message.chat.id, message.text)
-            bot.send_message(message.chat.id, "Ви пройшли реєстрацію")
-            markup.add(info, setting)
-            msg = bot.send_message(message.from_user.id, "Головне меню", reply_markup=markup)
-            bot.register_next_step_handler(msg, process_menu)
-        except:
-            bot.reply_to(message, "Помилка(")
+    elif (message.text == "Чоловік" or message.text == "Дівчина"):
+        database.add(BD, message.chat.id, message.text)
+        bot.send_message(message.chat.id, "Ви пройшли реєстрацію")
+        markup.add(info, setting)
+        msg = bot.send_message(message.from_user.id, "Головне меню", reply_markup=markup)
+        bot.register_next_step_handler(msg, process_menu)
     else:
         markup.add(male, female, back)
         msg = bot.send_message(message.from_user.id, "Оберіть правильну стать", reply_markup=markup)
@@ -107,22 +101,18 @@ def process_gender(message):
 
     
 def process_menu(message):
-    try:
-        chat_id = message.chat.id
-        if message.text == "Інфа про мене":
-            markup.add(back)
-            msg = bot.send_message(message.from_user.id, f'*Імʼя*: {BD[chat_id][0]}\n*Вік*: {BD[chat_id][1]}\n*Стать*: {BD[chat_id][2]}', parse_mode="MarkdownV2", reply_markup=markup)
-            bot.register_next_step_handler(msg, process_menu)
-        elif message.text == "Настройки":
-            markup.add(name, age, gender, back)
-            msg = bot.send_message(message.from_user.id, "Оберіть пункт що хочете змінити", reply_markup=markup)
-            bot.register_next_step_handler(msg, process_change)
-        elif message.text == "Назад":
-            markup.add(info, setting)
-            msg = bot.send_message(message.from_user.id, "Головне меню", reply_markup=markup)
-            bot.register_next_step_handler(msg, process_menu)
-    except:
-        bot.reply_to(message, "Помилка(")
+    if message.text == "Інфа про мене":
+        markup.add(back)
+        msg = bot.send_message(message.from_user.id, f'*Імʼя*: {BD[message.chat.id][0]}\n*Вік*: {BD[message.chat.id][1]}\n*Стать*: {BD[message.chat.id][2]}', parse_mode="MarkdownV2", reply_markup=markup)
+        bot.register_next_step_handler(msg, process_menu)
+    elif message.text == "Настройки":
+        markup.add(name, age, gender, back)
+        msg = bot.send_message(message.from_user.id, "Оберіть пункт що хочете змінити", reply_markup=markup)
+        bot.register_next_step_handler(msg, process_change)
+    elif message.text == "Назад":
+        markup.add(info, setting)
+        msg = bot.send_message(message.from_user.id, "Головне меню", reply_markup=markup)
+        bot.register_next_step_handler(msg, process_menu)
         
 def process_change(message):
     if message.text == 'Змінити вік':
@@ -171,14 +161,11 @@ def process_change_gender(message):
         markup.add(name, age, gender, back)
         msg = bot.send_message(message.from_user.id, "Оберіть пункт що хочете змінити", reply_markup=markup)
         bot.register_next_step_handler(msg, process_change)
-    elif message.text == "Чоловік" or message.text == "Дівчина":
-        try:
-            database.replace(BD, message.chat.id, message.text, 2)
-            markup.add(name, age, gender, back)
-            msg = bot.send_message(message.from_user.id, "Оберіть пункт що хочете змінити", reply_markup=markup)
-            bot.register_next_step_handler(msg, process_change)
-        except:
-            bot.reply_to(message, "Помилка(")
+    elif (message.text == "Чоловік" or message.text == "Дівчина"):
+        database.replace(BD, message.chat.id, message.text, 2)
+        markup.add(name, age, gender, back)
+        msg = bot.send_message(message.from_user.id, "Оберіть пункт що хочете змінити", reply_markup=markup)
+        bot.register_next_step_handler(msg, process_change)
     else:
         markup.add(male, female, back)
         msg = bot.send_message(message.from_user.id, "Оберіть правильну стать", reply_markup=markup)
@@ -190,18 +177,15 @@ def process_change_name(message):
         msg = bot.send_message(message.from_user.id, "Оберіть пункт що хочете змінити", reply_markup=markup)
         bot.register_next_step_handler(msg, process_change)
     else:
-        try:
-            if(len(message.text) >= 2 and len(message.text)<=20):
-                database.replace(BD, message.chat.id, message.text, 0)
-                markup.add(name, age, gender, back)
-                msg = bot.send_message(message.from_user.id, "Оберіть пункт що хочете змінити", reply_markup=markup)
-                bot.register_next_step_handler(msg, process_change)
-            else:
-                bot.reply_to(message, "Імʼя від 2 до 20 символів")
-                msg = bot.send_message(message.from_user.id, "Ваше Ім'я")
-                bot.register_next_step_handler(msg, process_change_name)
-        except:
-            bot.reply_to(message, "Помилка(")
+        if(len(message.text) >= 2 and len(message.text)<=20):
+            database.replace(BD, message.chat.id, message.text, 0)
+            markup.add(name, age, gender, back)
+            msg = bot.send_message(message.from_user.id, "Оберіть пункт що хочете змінити", reply_markup=markup)
+            bot.register_next_step_handler(msg, process_change)
+        else:
+            bot.reply_to(message, "Імʼя від 2 до 20 символів")
+            msg = bot.send_message(message.from_user.id, "Ваше Ім'я")
+            bot.register_next_step_handler(msg, process_change_name)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def get_message():
